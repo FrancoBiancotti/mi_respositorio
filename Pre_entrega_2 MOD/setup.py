b188ede8144968824876145e8a91a345dda4e534
Franco Biancotti <francobiancotti@gmail.com>
2023-08-13 20:17:58 -0300

    Primer commit

--- Pre_entrega_2 MOD/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="Compras",
-    version="0.1",
-    description="Modelo de entidades para compras",
-    author="Franco",
-    author_email="francobiancotti@gmail.com",
-    packages=["Compras"],)
